74f87d20d21b9ab3d291e294a05c44838d6b190c
aruizmolina <aruizmolina@unomaha.edu>
2024-08-28 16:41:06 +0000

    madlib crazyyy

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,22 +1,24 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Angel Ruiz Molina
+#Date: 8/28/24
+#Assignment: Lab 1
 
 def main():
-  print("First Program")
+  #print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
-
+  name = input("What is your name? ")
   #Use the user's name in the program.
-
+  print("Nice to meet you,", name)
   #Ask the user for their age.
-
+  age = input("How old are you? ")
   #Tell the user what year they were born in.
+  age = int(age)
+  birthYear = 2024 - age
   #Assume that they have not had their birthday yet this year.
-
-
+  print(birthYear)
+  
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
--- MadLib.py
+++ MadLib.py
@@ -6,11 +6,17 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input("Give me a noun ")
+  noun2 = input("Give me another noun ")
+  adj1 = input("Give me and adjective ")
+  verb1 = input("Give me a verb in the past tense ")
+  verb2 = input("Give me another verb in the past tense ")
+  adverb1 = input("Give me an adverb ")
   #Print the story with the user supplied words.
-
-
-
+  print("Once there was a great", noun1, "the great", noun1, "had a very big problem, his", noun2, "was very unhelpful")
+  print("The", noun1, verb1, "to the store to see if he could find a brand new", noun2)
+  print("The", noun1, "arrived to the store and found a", adj1, noun2, "surely this new", noun2, "is an improvement")
+  print("The", noun1, adverb1, verb2, "back to his home and lived happily ever after")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
